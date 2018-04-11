9c104232960c9dcab7edc97fa65f6a2ad669ac37
Angular Builds <circleci@angular.io>
2018-04-11 10:30:26 +0000

    8d48293 feat(@schematics/update): add registry flag

--- update/npm.d.ts
+++ update/npm.d.ts
@@ -11,8 +11,9 @@ import { NpmRepositoryPackageJson } from './npm-package-json';
 /**
  * Get the NPM repository's package.json for a package. This is p
  * @param {string} packageName The package name to fetch.
+ * @param {string} registryUrl The NPM Registry URL to use.
  * @param {LoggerApi} logger A logger instance to log debug information.
  * @returns An observable that will put the pacakge.json content.
  * @private
  */
-export declare function getNpmPackageJson(packageName: string, logger: logging.LoggerApi): Observable<NpmRepositoryPackageJson>;
+export declare function getNpmPackageJson(packageName: string, registryUrl: string, logger: logging.LoggerApi): Observable<NpmRepositoryPackageJson>;
