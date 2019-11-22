27c6670802a508878cf85c87530cdb23aff26150
Angular Builds <circleci@angular.io>
2019-11-22 06:57:54 +0000

    a4a94a137 build: update core-js to version 3.4.2

--- update/index.js
+++ update/index.js
@@ -10,6 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@angular-devkit/core");
 const schematics_1 = require("@angular-devkit/schematics");
 const tasks_1 = require("@angular-devkit/schematics/tasks");
+const npa = require("npm-package-arg");
 const rxjs_1 = require("rxjs");
 const operators_1 = require("rxjs/operators");
 const semver = require("semver");
@@ -576,11 +577,11 @@ function _getAllDependencies(tree) {
     catch (e) {
         throw new schematics_1.SchematicsException('package.json could not be parsed: ' + e.message);
     }
-    return new Map([
+    return [
         ...Object.entries(packageJson.peerDependencies || {}),
         ...Object.entries(packageJson.devDependencies || {}),
         ...Object.entries(packageJson.dependencies || {}),
-    ]);
+    ];
 }
 function _formatVersion(version) {
     if (version === undefined) {
@@ -596,6 +597,15 @@ function _formatVersion(version) {
         throw new schematics_1.SchematicsException(`Invalid migration version: ${JSON.stringify(version)}`);
     }
     return version;
+}
+/**
+ * Returns whether or not the given package specifier (the value string in a
+ * `package.json` dependency) is hosted in the NPM registry.
+ * @throws When the specifier cannot be parsed.
+ */
+function isPkgFromRegistry(name, specifier) {
+    const result = npa.resolve(name, specifier);
+    return !!result.registry;
 }
 function default_1(options) {
     if (!options.packages) {
@@ -616,12 +626,21 @@ function default_1(options) {
     }
     options.from = _formatVersion(options.from);
     options.to = _formatVersion(options.to);
+    const usingYarn = options.packageManager === 'yarn';
     return (tree, context) => {
         const logger = context.logger;
-        const allDependencies = _getAllDependencies(tree);
-        const packages = _buildPackageList(options, allDependencies, logger);
-        const usingYarn = options.packageManager === 'yarn';
-        return rxjs_1.from([...allDependencies.keys()]).pipe(
+        const npmDeps = new Map(_getAllDependencies(tree).filter(([name, specifier]) => {
+            try {
+                return isPkgFromRegistry(name, specifier);
+            }
+            catch (_a) {
+                // Abort on failure because package.json is malformed.
+                throw new schematics_1.SchematicsException(`Failed to parse dependency "${name}" with specifier "${specifier}"`
+                    + ` from package.json. Is the specifier malformed?`);
+            }
+        }));
+        const packages = _buildPackageList(options, npmDeps, logger);
+        return rxjs_1.from(npmDeps.keys()).pipe(
         // Grab all package.json from the npm repository. This requires a lot of HTTP calls so we
         // try to parallelize as many as possible.
         operators_1.mergeMap(depName => npm_1.getNpmPackageJson(depName, logger, { registryUrl: options.registry, usingYarn, verbose: options.verbose })), 
@@ -656,14 +675,14 @@ function default_1(options) {
             do {
                 lastPackagesSize = packages.size;
                 npmPackageJsonMap.forEach((npmPackageJson) => {
-                    _addPackageGroup(tree, packages, allDependencies, npmPackageJson, logger);
-                    _addPeerDependencies(tree, packages, allDependencies, npmPackageJson, npmPackageJsonMap, logger);
+                    _addPackageGroup(tree, packages, npmDeps, npmPackageJson, logger);
+                    _addPeerDependencies(tree, packages, npmDeps, npmPackageJson, npmPackageJsonMap, logger);
                 });
             } while (packages.size > lastPackagesSize);
             // Build the PackageInfo for each module.
             const packageInfoMap = new Map();
             npmPackageJsonMap.forEach((npmPackageJson) => {
-                packageInfoMap.set(npmPackageJson.name, _buildPackageInfo(tree, packages, allDependencies, npmPackageJson, logger));
+                packageInfoMap.set(npmPackageJson.name, _buildPackageInfo(tree, packages, npmDeps, npmPackageJson, logger));
             });
             return packageInfoMap;
         }), operators_1.switchMap(infoMap => {
