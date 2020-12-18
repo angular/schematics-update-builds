470b3a2aa9918ee9d9993de10666030f81fbd2ba
Angular Builds <circleci@angular.io>
2020-12-18 08:05:24 +0000

    c0bc8be01 build: update angular packages

--- update/index.js
+++ update/index.js
@@ -389,7 +389,7 @@ function _usageMessage(options, infoMap, logger) {
         logger.info('  ' + fields.map((x, i) => x.padEnd(pads[i])).join(''));
     });
     logger.info(`\nThere might be additional packages which don't provide 'ng update' capabilities that are outdated.\n`
-        + `You can update the addition packages by running the update command of your package manager.`);
+        + `You can update the additional packages by running the update command of your package manager.`);
     return;
 }
 function _buildPackageInfo(tree, packages, allDependencies, npmPackageJson, logger) {
